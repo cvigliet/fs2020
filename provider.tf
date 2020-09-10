1c59fcee08d412d0d8da6107eb6522b5a68bee76
cvigliet <carina.viglietti@gmail.com>
2020-09-10 17:06:38 +0200

    Resource group by var

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "ibm" {
   generation            = 2
   region                = "${var.ibmcloud_region}"
-  resource_group        = "61a8434f8e714b698364ac038ade4204"
+  resource_group        = "${var.custom_resource_group}"
 }
